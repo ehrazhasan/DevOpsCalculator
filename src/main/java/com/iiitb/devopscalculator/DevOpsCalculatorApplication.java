d888f8cff68f095d6be4ecda0b45f2df82fc213d
ehrazhasan <ehraz_hasan@outlook.com>
2022-04-10 23:00:01 +0530

    Initial commit

---
+++ src/main/java/com/iiitb/devopscalculator/DevOpsCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.iiitb.devopscalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOpsCalculatorApplication.class, args);
+    }
+
+}
